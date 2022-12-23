423a3bf47ad52e35bac8d2cbc8b4943ce280ed54
Tagary <55379593+Tagary@users.noreply.github.com>
2022-12-23 12:42:11 +0700

    First init

---
+++ components/Navigation/index.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+import styles from './style.module.scss';
+
+interface NavigationProps {}
+
+export const Navigation: React.FC<NavigationProps> = ({}) => {
+  return <div>Navigation</div>;
+};
